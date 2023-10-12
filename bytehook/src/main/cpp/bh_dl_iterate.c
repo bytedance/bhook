cfe8020d9498e54fcd0a25cee4aa518df2e07baa
caikelun <caikelun@gmail.com>
2023-10-12 11:58:38 +0800

    Do clang format.

--- bytehook/src/main/cpp/bh_dl_iterate.c
+++ bytehook/src/main/cpp/bh_dl_iterate.c
@@ -115,7 +115,7 @@ static int bh_dl_iterate_by_maps(int (*callback)(struct dl_phdr_info *, size_t, 
 
   while (fgets(line, sizeof(buf1), maps)) {
     // Parsing maps directly has too much uncertainty, so it needs to be strict.
-    if(!bh_util_ends_with(line, BH_CONST_BASENAME_APP_PROCESS) && !bh_util_ends_with(line, ".so")) continue;
+    if (!bh_util_ends_with(line, BH_CONST_BASENAME_APP_PROCESS) && !bh_util_ends_with(line, ".so")) continue;
 
     uintptr_t base, offset;
     char exec;
