6cec22dd2ce1d98d9b72cc3a3b36374001332e3d
chengjian.scj <chengjian.scj@alibaba-inc.com>
2023-12-26 11:26:05 +0800

    Fix Android 4.x因未删除换行符导致无法解析/proc/self/maps的BUG

--- bytehook/src/main/cpp/bh_dl_iterate.c
+++ bytehook/src/main/cpp/bh_dl_iterate.c
@@ -114,6 +114,7 @@ static int bh_dl_iterate_by_maps(int (*callback)(struct dl_phdr_info *, size_t, 
   bool try_next_line = false;
 
   while (fgets(line, sizeof(buf1), maps)) {
+    bh_util_trim_ending(line);
     // Parsing maps directly has too much uncertainty, so it needs to be strict.
     if (!bh_util_ends_with(line, BH_CONST_BASENAME_APP_PROCESS) && !bh_util_ends_with(line, ".so")) continue;
 
